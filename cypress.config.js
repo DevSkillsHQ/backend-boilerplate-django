5006a92fe530ea02de6d1e87e14b0474e487b369
Ehsan Mirzarazi <ehsanmirzarazi@ehsans-mbp.home>
2024-02-20 11:46:31 +0100

    fix: supportFile added

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   env: {
     apiUrl: 'http://localhost:8000',
+    supportFile: false,
   },
   e2e: {
     setupNodeEvents(on, config) {},
